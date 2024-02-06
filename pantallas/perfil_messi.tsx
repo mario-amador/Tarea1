0ad843ed94f509994a80cd7f4af2129907838640
mario-amador <126843976+mario-amador@users.noreply.github.com>
2024-02-05 19:26:07 -0600

    Se modificó el perfil de Messi

--- pantallas/perfil_messi.tsx
+++ pantallas/perfil_messi.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {
 SafeAreaView,
@@ -13,8 +13,20 @@ Button,
 } from 'react-native';
 import { ScreenContext } from 'react-native-screens';
 import { useNavigation } from '@react-navigation/native';
+import Evaluacion from '../componets/evaluacion/Evaluacion';
 
 const Perfil_MESSI = ({ navigation }) => {
+
+  const [calificacion, setcalificacion] = useState (0);
+
+  const handleCalificacionChange = (valor:number) => {
+    if (calificacion == 1 &&  valor == 1){
+      setcalificacion(0);
+    } else {
+      setcalificacion(valor);
+    }
+  };
+
   const handleBackToMenu = () => {
     navigation.navigate('Home');
     };
@@ -28,6 +40,7 @@ const Perfil_MESSI = ({ navigation }) => {
               style={styles.fotoPerfil}
               source={require('../imagenes/messi-trofeo-ponton-08.jpg')}
             />
+            <Evaluacion calificacion={calificacion} onCalificacionChange={handleCalificacionChange} />
             <Text style={styles.encabezado}>Lionel Andrés Messi Cuccittini</Text>
             <View style={styles.seccion}>
               <Text style={styles.seccionHeader}>Lugar de Nacimiento:</Text>
@@ -64,7 +77,7 @@ const Perfil_MESSI = ({ navigation }) => {
     
     const styles = StyleSheet.create({
       encabezado: {
-        fontSize: 35,
+        fontSize: 23,
         fontWeight: 'bold',
         color: '#0000ff',
         marginTop: 10,
